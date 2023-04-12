b81305b0c7bcead543238d6dca346c29272d8c38
Milan Silic <silic.milan@gmail.com>
2023-04-13 01:26:36 +0200

    20230413

--- script/app.js
+++ script/app.js
@@ -1,46 +1,72 @@
+let iwW = document.getElementById('iwW'),
+    iwH = document.getElementById('iwH'),
+    owW = document.getElementById('owW'),
+    owH = document.getElementById('owH');
+
 const c2Num1 = document.getElementById('c2_num1'),
-      c2Num2 = document.getElementById('c2_num2'),
-      c2Num3 = document.getElementById('c2_num3'),
-      c2Num4 = document.getElementById('c2_num4'),
-      c1Num1 = document.getElementById('c1_num1'),
-      c1Num2 = document.getElementById('c1_num2'),
-      c1Num3 = document.getElementById('c1_num3');
-const calc1Changeables = [c2Num1, c2Num2, c2Num3];
-const calc2Changeables = [c1Num1, c1Num2, c1Num3];
+    c2Num2 = document.getElementById('c2_num2'),
+    c2Num3 = document.getElementById('c2_num3'),
+    c2Num4 = document.getElementById('c2_num4'),
+    c1Num1 = document.getElementById('c1_num1'),
+    c1Num2 = document.getElementById('c1_num2'),
+    c1Num3 = document.getElementById('c1_num3'),
+    c3Num1 = document.getElementById('c3_num1'),
+    c3Num2 = document.getElementById('c3_num2'),
+    c3Num3 = document.getElementById('c3_num3'),
+    calc1Values = [c1Num1, c1Num2, c1Num3],
+    calc2Values = [c2Num1, c2Num2, c2Num3];
 
-for (const c1Input of calc1Changeables) {
+
+// CALC 1
+for (const c1Input of calc1Values) {
     c1Input.oninput = () => {
-        c2Num4.value = (c2Num2.value * c2Num3.value) / c2Num1.value;
+        c1Num3.value = ((c1Num1.value / c1Num2.value) * 100).toFixed(3)
+    }
+}
+c1Num3.onselect = () => navigator.clipboard.writeText(c1Num3.value + 'vw')
+
+
+// CALC 2
+for (const c1Input of calc2Values) {
+    c1Input.oninput = () => {
+        c2Num4.value = (c2Num2.value * c2Num3.value) / c2Num1.value
+    }
+}
+c2Num4.onselect = () => navigator.clipboard.writeText(c2Num4.value)
+
+
+// CALC 3
+c3Num2.oninput = () => {
+    c3Num3.value = c3Num2.value / c3Num1.value;
+    if (Math.round(c3Num3.value) == c3Num3.value) {
+        c3Num3.value = (c3Num2.value / c3Num1.value).toFixed(0)
+    } else {
+        c3Num3.value = (c3Num2.value / c3Num1.value).toFixed(3)
     }
 }
 
-c2Num4.onselect = () => {
-    navigator.clipboard.writeText(c2Num4.value);
-}
-
-for (const c1Input of calc2Changeables) {
-    c1Input.oninput = () => {
-        c1Num3.value = ((c1Num1.value / c1Num2.value)*100).toFixed(3);
+c3Num3.oninput = () => {
+    c3Num2.value = c3Num3.value * c3Num1.value;
+    if (Math.round(c3Num2.value) == c3Num2.value) {
+        c3Num2.value = (c3Num3.value * c3Num1.value).toFixed(0)
+    } else {
+        c3Num2.value = (c3Num3.value * c3Num1.value).toFixed(3)
     }
 }
 
-c1Num3.onselect = () => {
-    navigator.clipboard.writeText(c1Num3.value + 'vw');
+c3Num2.onselect = () => navigator.clipboard.writeText(c3Num2.value + 'px')
+
+c3Num3.onselect = () => navigator.clipboard.writeText(c3Num3.value + 'rem')
+
+
+// get window dimensions
+const getWinDim = () => {
+    iwW.innerHTML = window.innerWidth;
+    iwH.innerHTML = window.innerHeight;
+    owW.innerHTML = window.outerWidth;
+    owH.innerHTML = window.outerHeight;
 }
 
-let aaa = document.getElementById('aaa');
-let bbb = document.getElementById('bbb');
-let ccc = document.getElementById('ccc');
-let ddd = document.getElementById('ddd');
-function mrnjau() {
-    aaa.innerHTML = window.innerWidth;
-    bbb.innerHTML = window.innerHeight;
-    ccc.innerHTML = window.outerWidth;
-    ddd.innerHTML = window.outerHeight;
-}
-mrnjau();
-
-window.onresize = mrnjau;
 
 // MASONERY PARENT HEIGHT CALCULATOR
 const main = document.getElementsByTagName('main');
@@ -48,10 +74,12 @@ let topics = [...document.getElementsByClassName('topic')];
 let topicHeights = 0;
 
 ['load', 'resize'].forEach(el => addEventListener(el, () => {
+    getWinDim();
     topicHeights = 0;
     for (const tpc of topics) { topicHeights = topicHeights + tpc.offsetHeight; }
-    main[0].style.height = (topicHeights / 4.34) + 'px';
+    main[0].style.height = (topicHeights / 4.5) + 'px';
 }));
 
-// SET SCREEN WIDTH FOR px to vw calculator
+
+// SET SCREEN WIDTH VALUE FOR px to vw calculator
 const setScreenWidth = vl => c1Num2.value = vl
