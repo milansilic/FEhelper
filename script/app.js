2a0b869697fd50c380969fada72392d9f3deab6a
Milan Silic <silic.milan@gmail.com>
2023-05-24 23:03:34 +0200

    20230524 update

--- script/app.js
+++ script/app.js
@@ -3,60 +3,50 @@ let iwW = document.getElementById('iwW'),
     owW = document.getElementById('owW'),
     owH = document.getElementById('owH');
 
-const c2Num1 = document.getElementById('c2_num1'),
-    c2Num2 = document.getElementById('c2_num2'),
-    c2Num3 = document.getElementById('c2_num3'),
-    c2Num4 = document.getElementById('c2_num4'),
-    c1Num1 = document.getElementById('c1_num1'),
+const c1Num1 = document.getElementById('c1_num1'),
     c1Num2 = document.getElementById('c1_num2'),
     c1Num3 = document.getElementById('c1_num3'),
+    c2Num1 = document.getElementById('c2_num1'),
+    c2Num2 = document.getElementById('c2_num2'),
+    c2Num3 = document.getElementById('c2_num3'),
     c3Num1 = document.getElementById('c3_num1'),
     c3Num2 = document.getElementById('c3_num2'),
     c3Num3 = document.getElementById('c3_num3'),
-    calc1Values = [c1Num1, c1Num2, c1Num3],
-    calc2Values = [c2Num1, c2Num2, c2Num3];
-
+    c3Num4 = document.getElementById('c3_num4'),
+    calc1Values = [c1Num1, c1Num2],
+    calc2Values = [c2Num1, c2Num2],
+    calc3Values = [c3Num1, c3Num2, c3Num3];
+    
 
 // CALC 1
-for (const c1Input of calc1Values) {
-    c1Input.oninput = () => {
+for (const input of calc1Values) {
+    input.oninput = () => {
         c1Num3.value = ((c1Num1.value / c1Num2.value) * 100).toFixed(3)
     }
 }
 c1Num3.onselect = () => navigator.clipboard.writeText(c1Num3.value + 'vw')
 
-
 // CALC 2
-for (const c1Input of calc2Values) {
-    c1Input.oninput = () => {
-        c2Num4.value = (c2Num2.value * c2Num3.value) / c2Num1.value
+for (const input of calc2Values) {
+    input.oninput = () => {
+        c2Num3.value = c2Num2.value / c2Num1.value;
+        if (Math.round(c2Num3.value) == c2Num3.value) {
+            c2Num3.value = (c2Num2.value / c2Num1.value).toFixed(0)
+        } else {
+            c2Num3.value = (c2Num2.value / c2Num1.value).toFixed(3)
+        }
     }
 }
-c2Num4.onselect = () => navigator.clipboard.writeText(c2Num4.value)
-
+c2Num2.onselect = () => navigator.clipboard.writeText(c2Num2.value + 'px')
+c2Num3.onselect = () => navigator.clipboard.writeText(c2Num3.value + 'rem')
 
 // CALC 3
-c3Num2.oninput = () => {
-    c3Num3.value = c3Num2.value / c3Num1.value;
-    if (Math.round(c3Num3.value) == c3Num3.value) {
-        c3Num3.value = (c3Num2.value / c3Num1.value).toFixed(0)
-    } else {
-        c3Num3.value = (c3Num2.value / c3Num1.value).toFixed(3)
+for (const input of calc3Values) {
+    input.oninput = () => {
+        c3Num4.value = (c3Num2.value * c3Num3.value) / c3Num1.value
     }
 }
-
-c3Num3.oninput = () => {
-    c3Num2.value = c3Num3.value * c3Num1.value;
-    if (Math.round(c3Num2.value) == c3Num2.value) {
-        c3Num2.value = (c3Num3.value * c3Num1.value).toFixed(0)
-    } else {
-        c3Num2.value = (c3Num3.value * c3Num1.value).toFixed(3)
-    }
-}
-
-c3Num2.onselect = () => navigator.clipboard.writeText(c3Num2.value + 'px')
-
-c3Num3.onselect = () => navigator.clipboard.writeText(c3Num3.value + 'rem')
+c3Num4.onselect = () => navigator.clipboard.writeText(c3Num4.value)
 
 
 // get window dimensions
@@ -82,4 +72,7 @@ let topicHeights = 0;
 
 
 // SET SCREEN WIDTH VALUE FOR px to vw calculator
-const setScreenWidth = vl => c1Num2.value = vl
+const setScreenWidth = vl => {
+    c1Num2.value = vl
+    c1Num3.value = ((c1Num1.value / c1Num2.value) * 100).toFixed(3)
+}
